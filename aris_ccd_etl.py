ba116dcd3818411e5e8cb9fedf26714f63bc17b3
Graham Chickering <grahamchickering@gmail.com>
2022-05-05 10:35:51 -0400

    updating dag

--- aris_ccd_etl.py
+++ aris_ccd_etl.py
@@ -382,11 +382,11 @@ gen_fiscal = PythonOperator(
 #     dag=dag
 # )
 
-# load_mrt_nonfiscal_district = PythonOperator(
-#     task_id = "load_mrt_nonfiscal_district",
-#     python_callable = mrt_nonfiscal_district,
-#     dag = dag
-# )
+load_mrt_nonfiscal_district = PythonOperator(
+    task_id = "load_mrt_nonfiscal_district",
+    python_callable = mrt_nonfiscal_district,
+    dag = dag
+)
 
 load_mrt_nonfiscal_state = PythonOperator(
     task_id = "load_mrt_nonfiscal_state",
@@ -414,7 +414,7 @@ load_mrt_nonfiscal_state = PythonOperator(
 # DAG Dependancy
 download_links >> download_dat 
 download_dat >> gen_nonfiscal >> gen_nonfiscal_wide >> gen_nonfiscal_school 
-download_dat >> gen_nonfiscal_district >> gen_district_wide 
+download_dat >> gen_nonfiscal_district >> gen_district_wide >> load_mrt_nonfiscal_district  
 download_dat >> gen_fiscal 
 gen_nonfiscal >> load_mrt_nonfiscal_state
 # >> load_mrt_nonfiscal_state
