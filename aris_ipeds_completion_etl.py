2be68a187142340bbd58ae800430ec3347fca301
Graham Chickering <grahamchickering@gmail.com>
2022-05-17 16:52:16 -0400

    updating completion pipeline

--- aris_ipeds_completion_etl.py
+++ aris_ipeds_completion_etl.py
@@ -47,6 +47,26 @@ def sas_completion():
         if ssh_client:
             ssh_client.close()
 
+def mrt_completion():
+    '''
+    Purpose: execute write_mrt.py on command line to generate mrt from nonfiscal long and write to database. 
+    '''
+    ssh = SSHHook(ssh_conn_id="svc_202205_sasdev")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\DB-Generation' + ' && python write_mrt_completion.py' 
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()            
+
 # Generate Nonfiscal state from CCD Data with SAS
 gen_completion = PythonOperator(
     task_id='gen_completion',
@@ -54,5 +74,11 @@ gen_completion = PythonOperator(
     dag=dag
 )
 
+gen_completion_mrt = PythonOperator(
+    task_id='gen_completion_mrt',
+    python_callable=mrt_completion,
+    dag=dag
+)
+
 # DAG Dependancy
-gen_completion
+gen_completion >> gen_completion_mrt
