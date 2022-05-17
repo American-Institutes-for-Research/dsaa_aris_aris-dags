1bc0af3c0c118fa74ac8e6f8b8495ced0d468914
Graham Chickering <grahamchickering@gmail.com>
2022-05-17 13:21:02 -0400

    adding in last load_mrt script

--- aris_ccd_etl.py
+++ aris_ccd_etl.py
@@ -394,11 +394,11 @@ load_mrt_nonfiscal_state = PythonOperator(
     dag = dag
 )
 
-# load_mrt_fiscal_state = PythonOperator(
-#     task_id = "load_mrt_fiscal_state",
-#     python_callable = mrt_fiscal_state,
-#     dag = dag
-# )
+load_mrt_fiscal_state = PythonOperator(
+    task_id = "load_mrt_fiscal_state",
+    python_callable = mrt_fiscal_state,
+    dag = dag
+)
 load_mrt_nonfiscal_school = PythonOperator(
     task_id = "load_mrt_nonfiscal_school",
     python_callable = mrt_nonfiscal_school,
@@ -415,9 +415,9 @@ load_mrt_nonfiscal_school = PythonOperator(
 download_links >> download_dat 
 download_dat >> gen_nonfiscal >> gen_nonfiscal_wide >> gen_nonfiscal_school >> load_mrt_nonfiscal_school
 download_dat >> gen_nonfiscal_district >> gen_district_wide >> load_mrt_nonfiscal_district  
-download_dat >> gen_fiscal 
+download_dat >> gen_fiscal >> load_mrt_fiscal_state
 gen_nonfiscal >> load_mrt_nonfiscal_state
-# >> load_mrt_nonfiscal_state
+# 5 >> load_mrt_nonfiscal_state
 #2>> load_mrt_nonfiscal_school
 ##3 >> load_mrt_nonfiscal_district  
 ##4 >> load_mrt_fiscal_state
