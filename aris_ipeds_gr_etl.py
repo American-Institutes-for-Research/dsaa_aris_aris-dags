4113b7af8e4d4794c5c031242cc8a9ce4de81b33
Graham Chickering <grahamchickering@gmail.com>
2022-05-23 15:33:26 -0400

    adding in ipeds gr pipeiple

---
+++ aris_ipeds_gr_etl.py
@@ -0,0 +1,83 @@
+from datetime import timedelta, datetime
+from pickle import TRUE
+import airflow
+import code_executer
+from airflow import DAG
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
+SERVICE_GIT_DIR = 'C:\\ARIS\\autoDigest\\ipeds' # File housing ARIS repos on SAS server's C drive
+
+# default args
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['mtrihn@air.org', 'gchickering@air.org'],
+    'email_on_failure': TRUE,
+    'email_on_retry': False,
+    'start_date': datetime.now() - timedelta(minutes=20),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define Main DAG for CCD pipeline 
+dag = DAG(dag_id='aris_ipeds_gr_etl',
+          default_args=default_args,
+        #   schedule_interval='0,10,20,30,40,50 * * * *',
+          dagrun_timeout=timedelta(seconds=600))
+
+def sas_gr():
+    '''
+    Purpose: execute ccd_nonfiscal_state_RE2.sas on command line to generate nonfiscal long data from ccd data 
+    '''
+    ssh = SSHHook(ssh_conn_id="svc_202205_sasdev")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\SAS' + '\\d21'+ '\\GR Survey SAS code'+' && FOR %I in (*.sas) DO sas %I'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()
+
+def mrt_GR():
+    '''
+    Purpose: execute write_mrt.py on command line to generate mrt from nonfiscal long and write to database. 
+    '''
+    ssh = SSHHook(ssh_conn_id="svc_202205_sasdev")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\DB-Generation' + ' && python write_mrt_gr.py' 
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()   
+
+# Generate Nonfiscal state from CCD Data with SAS
+gen_gr = PythonOperator(
+    task_id='gen_sfa',
+    python_callable=sas_gr,
+    dag=dag
+)
+gen_gr_mrt = PythonOperator(
+    task_id = 'load_mrt_sfa',
+    python_callable=mrt_GR,
+    dag= dag
+)
+
+# DAG Dependancy
+gen_gr >> gen_gr_mrt
