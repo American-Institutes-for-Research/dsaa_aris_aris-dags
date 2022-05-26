a9b67427e1de7898e820623cdf0efcb93c22e830
Graham Chickering <grahamchickering@gmail.com>
2022-05-26 12:26:26 -0400

    adding in attempt to create emial

---
+++ aris_email_trial.py
@@ -0,0 +1,43 @@
+from datetime import timedelta, datetime
+from pickle import TRUE
+import airflow
+import code_executer
+from airflow import DAG
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
+from airflow.operators.email_operator import EmailOperator
+
+SERVICE_GIT_DIR = 'C:\\ARIS\\autoDigest\\ipeds' # File housing ARIS repos on SAS server's C drive
+
+# default args
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': [ 'gchickering@air.org'],
+    'email_on_failure': TRUE,
+    'email_on_retry': False,
+    'start_date': datetime.now() - timedelta(minutes=20),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define Main DAG for CCD pipeline 
+dag = DAG(dag_id='aris_email_trial',
+          default_args=default_args,
+        #   schedule_interval='0,10,20,30,40,50 * * * *',
+          dagrun_timeout=timedelta(seconds=600))
+
+
+
+
+send_email = EmailOperator( 
+task_id='send_email', 
+to='gchickering@air.org', 
+subject='ingestion complete', 
+html_content="Date: {{ ds }}", 
+dag=dag
+)
+
+send_email
