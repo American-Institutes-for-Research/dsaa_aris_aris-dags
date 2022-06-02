092a929a6dbbe7d3ff00ad90588ef99a06a8d692
Graham Chickering <grahamchickering@gmail.com>
2022-06-02 12:07:49 -0400

    trying out file parser

--- aris_ipeds_sfa_etl.py
+++ aris_ipeds_sfa_etl.py
@@ -6,6 +6,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.python import ShortCircuitOperator
+
 from airflow.contrib.hooks.ssh_hook import SSHHook
 SERVICE_GIT_DIR = 'C:\\ARIS\\autoDigest\\ipeds' # File housing ARIS repos on SAS server's C drive
 
@@ -67,6 +69,25 @@ def mrt_SFA():
         if ssh_client:
             ssh_client.close()   
 
+def sas_log_check():
+
+    ssh = SSHHook(ssh_conn_id="svc_202205_sasdev")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\SAS' + '\\d21'+ 'python .\sas_parser.py "SFA Survey SAS code" '
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()   
+
+
 # Generate Nonfiscal state from CCD Data with SAS
 gen_sfa = PythonOperator(
     task_id='gen_sfa',
@@ -79,5 +100,12 @@ gen_sfa_mrt = PythonOperator(
     dag= dag
 )
 
+sas_log_parser = ShortCircuitOperator(
+        task_id="check_sas_scripts",
+        python_callable=sas_log_check,
+        op_kwargs={},
+    )
+
 # DAG Dependancy
-gen_sfa >> gen_sfa_mrt
+#gen_sfa >> gen_sfa_mrt
+gen_sfa >> sas_log_parser
