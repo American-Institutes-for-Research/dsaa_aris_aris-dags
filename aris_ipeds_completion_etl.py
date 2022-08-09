70f99b5b225e9440abbd6f6f2118a33ac0827076
Graham Chickering <grahamchickering@gmail.com>
2022-08-09 11:03:24 -0400

    updating script aris

--- aris_ipeds_completion_etl.py
+++ aris_ipeds_completion_etl.py
@@ -53,13 +53,17 @@ def connect_to_server(run_command):
 
 def compile_sas_command(sas_arguments):
     print(sas_arguments)
+
+    for sas_key in sas_arguments:
+        print(sas_key)
+        command_str = "sas" + sas_key
     # for sas_key in sas_arguments:
     #     command_str = "sas" + sas_key 
     #     for key, value in sas_key:
     #         argument_str = " -set " + key + " " + value
     #         print(key , "->", value)
     #         command_str = command_str + argument_str
-    # print(command_str)
+    print(command_str)
 
 
 
